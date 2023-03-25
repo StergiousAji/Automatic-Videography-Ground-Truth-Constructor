40766f799fcd19727264338069efeb626827122c
Stergious Aji <2546916A@student.gla.ac.uk>
2023-03-25 14:17:08 +0000

    Added CSRF resolution (again)

--- mysite/settings.py
+++ mysite/settings.py
@@ -64,6 +64,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
